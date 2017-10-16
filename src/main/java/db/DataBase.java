4858b56dc4e21b31dd4b2e0bf99e8d8cfbeaa1ab
wheejuni <wheejuni@gmail.com>
2017-10-16 14:13:04 +0900

    PR 해보기

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -21,4 +21,8 @@ public class DataBase {
     public static Collection<User> findAll() {
         return users.values();
     }
+    
+    public static String HelloPobi() {
+    		return "hello pobi";
+    }
 }
