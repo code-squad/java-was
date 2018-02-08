a23882870b861d10577d4ccacc383f775cef8b03
이끼룩 <saram4030@gmail.com>
2018-02-08 11:40:45 +0900

    [ADD] 요구사항 3 구현 - Request body 값 추출

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpRequestUtils {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestUtils.class);
     /**
-     * @param queryString은
-     *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+     * @param queryString URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
      */
     public static Map<String, String> parseQueryString(String queryString) {
@@ -18,8 +20,7 @@ public class HttpRequestUtils {
     }
 
     /**
-     * @param 쿠키
-     *            값은 name1=value1; name2=value2 형식임
+     * @param cookies 쿠키값은 name1=value1; name2=value2 형식임
      * @return
      */
     public static Map<String, String> parseCookies(String cookies) {
@@ -42,6 +43,7 @@ public class HttpRequestUtils {
         }
 
         String[] tokens = keyValue.split(regex);
+
         if (tokens.length != 2) {
             return null;
         }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,10 +4,12 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RequestHandler extends Thread {
@@ -25,38 +27,73 @@ public class RequestHandler extends Thread {
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-			String line = bufferedReader.readLine();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
 			if (line == null) {
 				return;
 			}
 
-			log.debug("line: {}", line);
 			String[] tokens = line.split(" ");
 			String method = tokens[0];
-			String[] fullURL = tokens[1].split("\\?");
+			String[] url = tokens[1].split("\\?");
+			String path = url[0];
+			log.debug("method: {}, path: {}", method, path);
+
+			Map<String, String> headerParams = new HashMap<>();
 
 			while (!line.isEmpty()) {
-				line = bufferedReader.readLine();
+				line = br.readLine();
+				if (line.isEmpty()) {
+					break;
+				}
+				log.debug("line: {}", line);
+
+				HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
+				if (pair != null) {
+					headerParams.put(pair.getKey(), pair.getValue());
+				}
 			}
 
-			String url = fullURL[0];
-			String query = fullURL[1];
-
-			Map<String, String> params = HttpRequestUtils.parseQueryString(query);
-			log.debug("query: {}", params);
-
-			User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
-			log.debug("user: {}", user);
+			switch (method) {
+				case "GET":
+					String query = (url.length > 1) ? url[1] : "";
+					dispatchHttpGet(path, query);
+					break;
+				case "POST":
+					String body = IOUtils.readData(br, Integer.parseInt(headerParams.get("Content-Length")));
+					dispatchHttpPost(path, body);
+					break;
+			}
 
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+			byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
 
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private void dispatchHttpGet(String path, String query) {
+		switch (path) {
+			case "/user/create":
+				createUserFromParams(HttpRequestUtils.parseQueryString(query));
+				break;
+		}
+	}
+
+	private void dispatchHttpPost(String path, String body) {
+		switch (path) {
+			case "/user/create":
+				createUserFromParams(HttpRequestUtils.parseQueryString(body));
+				break;
+		}
+	}
+
+	private void createUserFromParams(Map<String, String> params) {
+		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+		log.debug("user: {}", user);
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
