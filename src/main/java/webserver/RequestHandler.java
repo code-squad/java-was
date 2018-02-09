f2db6845aa3a00e5e08ef9c898f7398a70a230fe
HannaJeon <jks7psj@gmail.com>
2018-02-09 10:49:11 +0900

    요구사항5 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,8 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,46 +28,84 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
             String line = br.readLine();
-            String url = HttpRequestUtils.parseUrl(line);
-            int contentLength = 0;
-            String requestBody;
 
             if (line == null)
                 return;
+
+            String[] url = line.split(" ");
+            String HttpMethod = url[0];
+            String path = url[1];
+
+            Map<String, String> header = new HashMap<>();
 
             while (!line.equals("")) {
                 System.out.println(line);
                 line = br.readLine();
 
-                HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
-                if (pair != null && pair.getKey().equals("Content-Length")) {
-                    contentLength = Integer.parseInt(pair.getValue());
+                if (!line.equals("")) {
+                    String[] keyValue = line.split(": ");
+                    header.put(keyValue[0], keyValue[1]);
                 }
             }
 
-            requestBody = IOUtils.readData(br,contentLength);
-
-            DataOutputStream dos = new DataOutputStream(out);
-
-            if (!requestBody.equals("")) {
-                queryToModel(requestBody);
-                response302Header(dos);
-                return;
+            switch (HttpMethod) {
+                case "GET":
+                    get(out, path);
+                    break;
+                case "POST":
+                    post(br, out, header, path);
+                    break;
             }
 
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void queryToModel(String body) {
+    private void get(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void post(BufferedReader br, OutputStream out, Map<String, String> header, String path) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        int contentLength = Integer.parseInt(header.get("Content-Length"));
+        String requestBody = IOUtils.readData(br, contentLength);
+        switch (path) {
+            case "/user/create":
+                saveUser(requestBody);
+                response302Header(dos);
+                break;
+            case "/user/login":
+                response302Header(dos);
+                login(requestBody, dos);
+                break;
+        }
+    }
+
+    private void saveUser(String body) {
         Map<String, String> map = HttpRequestUtils.parseQueryString(body);
         User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
-        log.debug("useruser: {}", user);
+        DataBase.addUser(user);
+    }
+
+    private DataOutputStream login(String body, DataOutputStream dos) throws IOException {
+        Map<String, String> map = HttpRequestUtils.parseQueryString(body);
+        User user = DataBase.findUserById(map.get("userId"));
+        if (user == null || !user.getPassword().equals(map.get("password"))) {
+            dos.writeBytes("Location: /user/login_failed.html\r\n");
+            dos.writeBytes("Set-Cookie: logined=false; Path=/ \r\n");
+            dos.writeBytes("\r\n");
+            return dos;
+        }
+        dos.writeBytes("Location: /index.html\r\n");
+        dos.writeBytes("Set-Cookie: logined=true; Path=/ \r\n");
+        dos.writeBytes("\r\n");
+        return dos;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -83,8 +123,6 @@ public class RequestHandler extends Thread {
         try {
             dos.writeBytes("HTTP/1.1 302 FOOUND \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Location: /index.html\r\n");
-            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
