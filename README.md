f9df2eed68db44616577fb16bd2f9893d2a29d87
Yejun Park <smartyjpark@gmail.com>
2018-02-09 11:39:04 +0900

    [Yejun] Step7 (#17)
    * Added: url 입력시 해당 경로의 html 파일을 전달
    
    * Fixed: read me
    
    * Fixed: readme file
    
    * Added: Function to create user by GET method
    
    * Added: Function to create user by POST method
    
    * Added: added method to redirect after handle post method
    
    * Added: user list 동적으로 만들어주는 기능 / 로그인 시에만 해당 페이지 get 가능
    
    * Added: response 에 content-type을 요청에 맞게 보내줌

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
-# 웹 애플리케이션 서버
-## 진행 방법
-* 웹 애플리케이션 서버 요구사항을 파악한다.
-* 요구사항에 대한 구현을 완료한 후 자신의 github 아이디에 해당하는 브랜치에 Pull Request(이하 PR)를 통해 코드 리뷰 요청을 한다.
-* 코드 리뷰 피드백에 대한 개선 작업을 하고 다시 PUSH한다.
-* 모든 피드백을 완료하면 다음 단계를 도전하고 앞의 과정을 반복한다.
+# WAS Mission
 
-## 온라인 코드 리뷰 과정
-* [텍스트와 이미지로 살펴보는 코드스쿼드의 온라인 코드 리뷰 과정](https://github.com/code-squad/codesquad-docs/blob/master/codereview/README.md)
-* [동영상으로 살펴보는 코드스쿼드의 온라인 코드 리뷰 과정](https://youtu.be/a5c9ku-_fok)
+## Step1
+
+- 브라우저에서 오는 모든 요청은 input stream에 담겨있다.
+- 서버에서 클라이언트에게 보내는 데이터는 outputStream에 담는다.
+-  데이터를 라인별로 읽는게 힘들어서 Buffered Reader 로 읽는다
+- byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath()) 형태로 response body 에 해당 경로의 파일 정보를 읽어서 클라이언트에게 전달한다.
+
+
+
+##  Step2
+
+- 프레임워크에서 자동으로 query 를 parsing 해주는건 정말 꿀같은 거였다...
+
+
+
+##  Step3
+
+- POST 메소드에서는 파라미터가 header 하단 공백 다음에 온다.
+
+
+
+## Step4
+
+- header에 Location 을 추가해서 보내줌으로써 post 메소드의 redirectURL 을 설정할 수 있다.
+
+
+
+## Step5
+
+- Set-Cookie 를 통해 유저의 로그인 상태를 확인할 수 있다.
+- Set-Cooke 를 설정해서 클라이언트에게 응답을 요청하면, 클라이언트는 다시 cookie 에 로그인 상태를 담아서 request를 보낸다.
+
+
+
+## Step6
+
+- Handlebars가 그립다.. 템플릿 적용하는 것이 어렵다.
+
+
+
+## Step7
+
+- 서버 - 브라우져의 통신에서 Content-Type 을 설정해주지 않으면(또는 잘못설정하면) 브라우져는 인식을 하지 못한다.
+
+
+
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,20 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,21 +30,137 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String[] firstLines = br.readLine().split(" ");
+            String method = firstLines[0];
+            String requestUrl = firstLines[1];
+            HashMap<String, String> headerMap = getHeaderObject(br);
+            log.debug("it's method : {}", method);
+
+            if (method.equals("POST")) responsePost(br, out, headerMap, requestUrl);
+
+            if (method.equals("GET")) responseGet(requestUrl, out, headerMap);
+
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void responseGet(String query, OutputStream out, HashMap<String, String> headers) throws IOException {
+        String url = (query.equals("/")) ? "/index.html" : query;
+
+        byte[] body = null;
+
+        if (query.equals("/user/list") || query.equals("/user/list.html") ) {
+            if (isLogined(headers)) {
+                body = createHtml(query);
+            }
+        }
+
+        log.debug("request url : {}", url);
+        DataOutputStream dos = new DataOutputStream(out);
+        if (body == null) body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length, headers);
+        responseBody(dos, body);
+    }
+
+    private boolean isLogined(HashMap<String, String> headers){
+        boolean logined = false;
+        if (headers.containsKey("Cookie")){
+            log.debug("it's get islogined: {}", headers.get("Cookie"));
+            logined = headers.get("Cookie").contains("logined=true");
+        }
+
+        return logined;
+    }
+
+    private byte[] createHtml(String url) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        List<String> lines = Files.readAllLines(Paths.get("./webapp/user/list.html"));
+        lines.forEach(s -> builder.append(s.replace("{{user}}", createUserList(DataBase.findAll()))));
+        return builder.toString().getBytes();
+    }
+
+    private String createUserList(Collection<User> users) {
+        if (users.size() == 0) return "";
+        StringBuilder builder = new StringBuilder();
+        users.forEach(user -> {
+            builder.append(
+                    String.format("<tr>\n" +
+                            "<th scope=\"row\">1</th><td>%s</td> <td>%s</td> <td>%s</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
+                            "</tr>", user.getUserId(), user.getName(), user.getEmail()));
+        });
+
+        return builder.toString();
+    }
+
+    private void responsePost(BufferedReader br, OutputStream out, HashMap<String, String> headers, String requestUrl) throws IOException {
+        Map<String, String> queryMap = HttpRequestUtils.parseQueryString(IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length"))));
+        DataOutputStream dos = new DataOutputStream(out);
+        String redirectUrl = "/index.html";
+        boolean logined = isLogined(headers);
+        switch (requestUrl) {
+            case "/user/create":
+                createUser(queryMap);
+                break;
+            case "/user/login":
+                logined = login(queryMap.get("userId"), queryMap.get("password"));
+                redirectUrl = (logined) ? "/index.html" : "/user/login_failed.html";
+        }
+
+        response302Header(dos, redirectUrl, logined);
+        responseBody(dos, new byte[0]);
+    }
+
+    private void createUser(Map<String, String> parameter) {
+        User newUser = new User(parameter.get("userId"), parameter.get("password"), parameter.get("name"), parameter.get("email"));
+        DataBase.addUser(newUser);
+        log.debug("created user : {}", newUser);
+    }
+
+    private boolean login(String userId, String password) {
+        if (!DataBase.findUserById(userId).getPassword().equals(password)) {
+            return false;
+        }
+        return true;
+    }
+
+    private HashMap<String, String> getHeaderObject(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        HashMap<String, String> pairs = new HashMap<>();
+        while (!line.isEmpty()) {
+            line = br.readLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            log.debug("line: {}", line);
+
+            HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
+            if (pair != null) {
+                pairs.put(pair.getKey(), pair.getValue());
+            }
+        }
+        return pairs;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, HashMap<String, String> headers) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(String.format("Content-Type: %s;charset=utf-8\r\n", headers.get("Accept").split(",")[0]));
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes(String.format("Set-Cookie: logined=%s; Path=/ \r\n", (isLogined(headers)) ? "true" : "false"));
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String redirectUrl, boolean logined) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes(String.format("Set-Cookie: logined=%s; Path=/ \r\n", (logined) ? "true" : "false"));
+            dos.writeBytes(String.format("Location: %s \r\n", redirectUrl));
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
