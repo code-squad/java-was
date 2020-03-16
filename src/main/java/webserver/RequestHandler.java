e6ad5629ed8ec191f93676f5fb31867cef454bc1
Jay <beginin15@gmail.com>
2020-03-16 17:18:57 +0900

    [1단계] path 분리
    /index.html 요청시, split 하여 정상적으로 출력된다.
    IOUtils parsePath method를 통해 위 요청사항을 해결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -26,6 +27,9 @@ public class RequestHandler extends Thread {
             String line = br.readLine();
             if (line == null)
                 return;
+
+            String path = IOUtils.parsePath(line);
+            log.debug("path : {} ", path);
             while (!"".equals(line)) {
                 log.debug("br : {} ", line);
                 line = br.readLine();
