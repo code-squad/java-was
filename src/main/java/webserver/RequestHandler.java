cac0a794ca40034bc82e85923821c3372474c81b
han95210 <han95210@naver.com>
2020-03-16 18:01:25 +0900

    refactor : Handlebar 수정
    - 모든 Request Header 출력하기
    - Request Line에서 path 분리하기
    - path에 해당하는 파일 읽어 응답하기
    
    associate with : #1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,12 +21,12 @@ public class RequestHandler extends Thread {
 
   public void run() {
     log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-              connection.getPort());
+        connection.getPort());
 
     try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
       // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
       DataOutputStream dos = new DataOutputStream(out);
-      byte[] body = "Hello World".getBytes();
+//      byte[] body = "Hello World".getBytes();
       //            log.debug("### run");
 
       BufferedReader buffer = new BufferedReader(new InputStreamReader(in, "UTF-8"));
@@ -36,7 +38,15 @@ public class RequestHandler extends Thread {
       //      }
 
       List<String> inputList = buffer.lines().collect(Collectors.toList());
+      String[] requestLine = inputList.get(0).split(" ");
+      String requestUrl =requestLine[1];
 
+      log.debug("### : {}", requestUrl);
+
+      File requestFile = new File("./webapp" + requestUrl);
+      byte[] body = Files.readAllBytes(requestFile.toPath());
+
+      log.debug("### body :  {}", new String(body));
       log.debug("### : end of request");
 
       response200Header(dos, body.length);
