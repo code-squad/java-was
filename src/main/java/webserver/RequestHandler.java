a38a6586b44966c2b8b0bc802bde215b8dd383d7
blossun <dnsngu@gmail.com>
2020-03-17 15:58:33 +0900

    [Feat] : 로그인 기능 구현
    findUserById를 옵셔널로 변경
    요청 URL에 따라서 다른 동작을 할 수 있도록 수정
    Refactoring 필요함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,27 +40,51 @@ public class RequestHandler extends Thread {
                 DataOutputStream dos = new DataOutputStream(out);
                 response200Header(dos, body.length);
                 responseBody(dos, body);
-            }
+            } else if (httpMethod.equals("POST")) {
+                if (url.contains("create")) {
+                    int contentLength = 0;
 
-            int contentLength = 0;
+                    while (true) {
+                        String line = br.readLine();
+                        if ("".equals(line)) {
+                            break;
+                        }
+                        if(line.contains("Content-Length")) {
+                            contentLength = Integer.parseInt(line.split(": ")[1]);
+                        }
+                    }
+                    String userParameter = IOUtils.readData(br, contentLength);
+                    Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(userParameter);
+                    User newUser = new User(parameterMap);
+                    DataBase.addUser(newUser);
+                    log.debug("user : {}", newUser);
+                    DataOutputStream dos = new DataOutputStream(out);
+                    String redirectUrl = "/index.html";
+                    response302Header(dos, redirectUrl);
+                } else if (url.contains("login")) {
+                    int contentLength = 0;
 
-            while (true) {
-                String line = br.readLine();
-                if ("".equals(line)) {
-                    break;
+                    while (true) {
+                        String line = br.readLine();
+                        if ("".equals(line)) {
+                            break;
+                        }
+                        if(line.contains("Content-Length")) {
+                            contentLength = Integer.parseInt(line.split(": ")[1]);
+                        }
+                    }
+                    String userParameter = IOUtils.readData(br, contentLength);
+                    Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(userParameter);
+                    User loginUser = DataBase.findUserById(parameterMap.get("userId")).orElse(null);
+                    String inputPassword = parameterMap.get("password");
+                    String setCookie = "logined=false;";
+                    if (loginUser != null && loginUser.getPassword().equals(inputPassword)) {
+                        setCookie = "logined=true;";
+                    }
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response200Header(dos, setCookie);
                 }
-                if(line.contains("Content-Length")) {
-                    contentLength = Integer.parseInt(line.split(": ")[1]);
-                }
             }
-            String userParameter = IOUtils.readData(br, contentLength);
-            Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(userParameter);
-            User newUser = new User(parameterMap);
-            log.debug("user : {}", newUser);
-
-            DataOutputStream dos = new DataOutputStream(out);
-            String redirectUrl = "/index.html";
-            response302Header(dos, redirectUrl);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -70,6 +95,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    private void response200Header(DataOutputStream dos, String setCookie) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Set-Cookie: " + setCookie + " Path=/\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
