35daff4f1f9f1ccb567b12702708c1127264d3d2
Byung Hyun Choi <choi8608@gmail.com>
2020-03-18 02:58:22 +0900

    refactor : RequestHandler 개선
    - requestLine, requestHeader, requestBody 를 가져올 수 있는 메소드를 나누어 구현
    - statusLine, responseHeader, responseBody 를 가져올 수 있는 메소드를 나누어 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -36,26 +36,20 @@ public class RequestHandler extends Thread {
       BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
       Map<String, Pair> requestLine = requestLine(br);
-      log.debug("### requestLine : {}", requestLine);
       Map<String, Pair> requestHeader = requestHeader(br);
-      log.debug("### requestHeader : {}", requestHeader);
-
       String requestBody = "";
       if (requestHeader.containsKey("Content-Length")) {
-        log.debug("### Content-Length : {}", requestHeader.get("Content-Length").getValue());
-        requestBody = requestBody(br
-            , Integer.parseInt(requestHeader.get("Content-Length").getValue()));
-        log.debug("### requestBody : {}", requestBody);
+        requestBody = requestBody(br, Integer.parseInt(requestHeader.get("Content-Length").getValue()));
       }
 
       PageController pc = new PageController();
-      String redirectUrl = pc.doWork(requestLine, requestBody);
+      String responseBodyURL = pc.doWork(requestLine, requestBody);
 
-      File uriFile = new File(WEBAPP_PATH + redirectUrl);
-      byte[] body = Files.readAllBytes(uriFile.toPath());
+      String statusLine = statusLine(200);
+      byte[] body = responseBody(responseBodyURL);
+      String responseHeader = responseHeader(body.length);
 
-      response200Header(dos, body.length);
-      responseBody(dos, body);
+      sendResponse(dos, statusLine, responseHeader, body);
     } catch (IOException ie) {
       log.error(ie.getMessage());
     } catch (Exception e) {
@@ -64,11 +58,10 @@ public class RequestHandler extends Thread {
   }
 
   /**
-   * @param br
-   * @return
-   * @throws Exception
+   * Feat : parsing 된 requestLine 을 리턴해줍니다.
+   * Desc : parseRequestLine() 을 통해 method, requestUrl, protocol 로 parsing 됩니다.
+   * Return : Map<String, Pair>
    */
-
   private Map<String, Pair> requestLine(BufferedReader br) throws Exception {
     if (!br.ready()) return new HashMap<>();
 
@@ -79,6 +72,11 @@ public class RequestHandler extends Thread {
     return requestLine;
   }
 
+  /**
+   * Feat : parsing 된 requestHeader 를 리턴해줍니다.
+   * Desc : parseHeader() 를 통해 header 에 들어 있는 값들을 parsing 합니다.
+   * Return : Map<String, Pair>
+   */
   private Map<String, Pair> requestHeader(BufferedReader br) throws Exception {
     if (!br.ready()) return new HashMap<>();
 
@@ -94,30 +92,61 @@ public class RequestHandler extends Thread {
     return requestHeader;
   }
 
+  /**
+   * Feat : requestBody 를 가져옵니다.
+   * Desc : contentLength 만큼 가져옵니다.
+   * Return : String
+   */
   private String requestBody(BufferedReader br, int contentLength) throws Exception {
     return (br.ready()) ? IOUtils.readData(br, contentLength) : null;
   }
 
-  private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-    try {
-      log.debug("### response200Header, {}", lengthOfBodyContent);
-      dos.writeBytes("HTTP/1.1 200 OK \r\n");
-      dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-      dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-      dos.writeBytes("\r\n");
-
-    } catch (IOException e) {
-      log.error(e.getMessage());
-    }
+  /**
+   * Feat : statusLine 을 만듭니다.
+   * Desc :
+   * Return : String
+   */
+  private String statusLine(int statusCode) {
+    log.debug("### statusLine");
+    return "HTTP/1.1" + statusCode + " OK \r\n";
   }
 
-  private void responseBody(DataOutputStream dos, byte[] body) {
-    try {
-      log.debug("### DataOutputStream");
-      dos.write(body, 0, body.length);
-      dos.flush();
-    } catch (IOException e) {
-      log.error(e.getMessage());
-    }
+  /**
+   * Feat : responseHeader 를 만듭니다.
+   * Desc :
+   * Return : String
+   */
+  private String responseHeader(int lengthOfBodyContent) {
+    log.debug("### responseHeader, {}", lengthOfBodyContent);
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("Content-Type: text/html;charset=utf-8\r\n");
+    sb.append("Content-Length: ").append(lengthOfBodyContent).append("\r\n");
+    sb.append("\r\n");
+
+    return sb.toString();
+  }
+
+  /**
+   * Feat : responseURL 을 가지고 responseBody 를 만듭니다.
+   * Desc :
+   * Return : byte[]
+   */
+  private byte[] responseBody(String responseBodyURL) throws Exception {
+    File uriFile = new File(WEBAPP_PATH + responseBodyURL);
+    return Files.readAllBytes(uriFile.toPath());
+  }
+
+  /**
+   * Feat : reponse 를 client 에게 보내줍니다.
+   * Desc :
+   * Return : void
+   */
+  private void sendResponse(DataOutputStream dos, String statusLine, String header, byte[] body) throws Exception {
+    log.debug("### DataOutputStream");
+    dos.writeBytes(statusLine);
+    dos.writeBytes(header);
+    dos.write(body, 0, body.length);
+    dos.flush();
   }
 }
